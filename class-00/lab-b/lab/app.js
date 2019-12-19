15c388dfc917909f2e8ec1a4c66c58314c9f88bc
alaamahasneh <alaa.ibraheem97@yahoo.com>
2019-12-19 16:44:55 +0200

    solve step 10

--- class-00/lab-b/lab/app.js
+++ class-00/lab-b/lab/app.js
@@ -108,7 +108,7 @@ console.log('Hello from the new object function', newObject(['hi', 'hello', 'are
 
 
 
-let sum = function(a, b, c, d) {
+let sum = (a, b, c, d )=> {
   return a + b + c + d;
 };
 
@@ -116,7 +116,7 @@ let sum = function(a, b, c, d) {
 console.log(sum(1, 2, 3, 4));
 
 
-let objectLit = function() {
+let objectLit = () => {
   return {
     key1: 'value1',
     key2: 'value2',
@@ -128,7 +128,7 @@ let objectLit = function() {
 console.log(objectLit());
 
 
-let sumAndProduct = function(a, b) {
+let sumAndProduct=(a, b) =>{
   let sum = a + b;
   let product = a * b;
   return [sum, product];
@@ -138,7 +138,7 @@ let sumAndProduct = function(a, b) {
 console.log(sumAndProduct(3, 9));
 
 
-let message = function(name) {
+let message = (name)=> {
   return `Hello, ${name}!`;
 };
 
